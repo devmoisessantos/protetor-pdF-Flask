7b47694239f56f0f284d12619cb711bf379b606f
devmoisessantos <062moises@gmail.com>
2025-01-08 13:16:51 -0300

    update - Refatorando o arquivo de inicializaçao main.py

--- main.py
+++ main.py
@@ -9,7 +9,12 @@ def create_app():
     # Definindo configurações diretamente no main.py
     app.config['SECRET_KEY'] = '6r45yera5y4e85y8e5'
 
-    app.config['UPLOAD_FOLDER'] = os.path.abspath('./upload/')
+    # Caminho absoluto para a pasta de uploads
+    upload_folder = os.path.abspath('./uploads/')
+    if not os.path.exists(upload_folder):
+        os.makedirs(upload_folder)
+
+    app.config['UPLOAD_FOLDER'] = upload_folder
 
     # Registrando o Blueprint
     app.register_blueprint(route_bp)
